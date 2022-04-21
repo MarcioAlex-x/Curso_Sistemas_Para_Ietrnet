011d0a5f95fd960b8afb6de468e1718c249effa5
Márcio Alex <marcioalex586@gmail.com>
2022-04-21 20:08:21 -0300

    conclusão

--- script.js
+++ script.js
@@ -1,2 +1,58 @@
-document.getElementById("art1");
-    
+var art1 = document.getElementById("art1")
+art1.addEventListener("mouseover",function(){
+    this.innerText = "Use a sua nota do ENEM de até CINCO ANOS para se matricular no curso que mais emprega em todo mundo"
+    this.style.fontSize = "20px"
+    this.style.backgroundColor = "#F2E718"
+    this.style.color = "black"
+    //this.style.transition = "0.4s"
+    this.style.fontWeight  = "100"
+})
+var art1 = document.getElementById("art1")
+art1.addEventListener("mouseout",function(){
+    this.innerText = "ENEM"
+    this.style.fontWeight  = "600"
+    this.style.fontSize = "30px"
+    this.style.backgroundColor = "#6A2B86"
+    this.style.color = "#E21622"
+    this.style.color = "white"
+    //this.style.transition = "0.5s"
+})
+
+var art2 = document.getElementById("art2")
+art2.addEventListener("mouseover",function(){
+    this.innerText = "Oferecemos bolsas de ESTÁGIO nas melhores empresas de tecnologia do mundo, com até 80% de efetivação entre os bolsistas"
+    this.style.fontSize = "20px"
+    this.style.backgroundColor = "#F2E718"
+    this.style.color = "black"
+    //this.style.transition = "0.4s"
+    this.style.fontWeight  = "100"
+})
+var art2 = document.getElementById("art2")
+art2.addEventListener("mouseout",function(){
+    this.innerText = "NOSSAS BOLSAS"
+    this.style.fontWeight  = "600"
+    this.style.fontSize = "30px"
+    this.style.backgroundColor = "#6A2B86"
+    this.style.color = "#E21622"
+    this.style.color = "white"
+    //this.style.transition = "0.5s"
+})
+var art3 = document.getElementById("art3")
+art3.addEventListener("mouseover",function(){
+    this.innerText = "Estude de casa por um preço que cabe no seu bolso sem perder as vantagens de ser aluno presencial "
+    this.style.fontSize = "20px"
+    this.style.backgroundColor = "#F2E718"
+    this.style.color = "black"
+    //this.style.transition = "0.4s"
+    this.style.fontWeight  = "100"
+})
+var art3 = document.getElementById("art3")
+art3.addEventListener("mouseout",function(){
+    this.innerText = "EAD"
+    this.style.fontWeight  = "600"
+    this.style.fontSize = "30px"
+    this.style.backgroundColor = "#6A2B86"
+    this.style.color = "#E21622"
+    this.style.color = "white"
+    //this.style.transition = "0.5s"
+})
